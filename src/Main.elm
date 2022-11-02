3ef9dda4495d0945856080ca327e3661302cc60a
Milton <milton94@gmail.com>
2022-11-02 20:31:20 +0000

    Basic mechanics for adding and viewing list items

--- src/Main.elm
+++ src/Main.elm
@@ -4,6 +4,8 @@ import Browser
 import Browser.Navigation as Nav
 import Html exposing (Html)
 import Html.Attributes as Attributes
+import Html.Events as Events
+import List
 import Random
 import Task
 import Time
@@ -32,22 +34,70 @@ main =
 --     ( [], Cmd.none )
 
 
-view : Model -> Html.Html msg
+view : Model -> Html.Html Msg
 view model =
     Html.div
-        []
-        [ todoForm
+        [ Attributes.class "main" ]
+        [ case model.view of
+            Home ->
+                Html.div []
+                    [ Html.div [] (List.map renderListItem model.todos)
+                    , Html.div [] [ newListItemButton ]
+                    ]
+
+            NewListItem ->
+                todoForm model
         ]
 
 
-todoForm : Html.Html msg
-todoForm =
-    Html.div [] []
+renderListItem : TodoItem -> Html Msg
+renderListItem todoList =
+    Html.div
+        []
+        [ Html.text todoList.title ]
+
+
+newListItemButton : Html Msg
+newListItemButton =
+    Html.div
+        []
+        [ Html.button [ Events.onClick NewTodo ] [ Html.text "Add new todo item" ]
+        ]
+
+
+todoForm : Model -> Html.Html Msg
+todoForm model =
+    Html.div
+        []
+        [ textInput "text" "Todo" model.newTodo TodoInput
+        , Html.button [ Events.onClick AddTodo ] [ Html.text "create" ]
+        ]
+
+
+textInput : String -> String -> String -> (String -> Msg) -> Html Msg
+textInput typeOfInput placeholder value toMsg =
+    Html.input [ Attributes.type_ typeOfInput, Attributes.placeholder placeholder, Attributes.value value, Events.onInput toMsg ] []
 
 
 update : Msg -> Model -> Model
 update msg model =
-    model
+    case msg of
+        NewTodo ->
+            { model | view = NewListItem }
+
+        TodoInput text ->
+            { model | newTodo = text }
+
+        AddTodo ->
+            { model
+                | todos =
+                    (::) { title = model.newTodo, status = Pending } model.todos
+                , newTodo = ""
+                , view = Home
+            }
+
+        _ ->
+            model
 
 
 subscriptions : Model -> Sub Msg
@@ -57,29 +107,46 @@ subscriptions model =
 
 type Msg
     = NewTodo
+    | TodoInput String
     | AddTodo
     | DeleteTodo
     | CancelTodo
 
 
+type Page
+    = Home
+    | NewListItem
 
+
+type TodoStatus
+    = Done
+    | Pending
+
+
+
+-- | ListView
 -- | LinkClicked Browser.UrlRequest
 -- | UrlChanged Url.Url
 
 
 type alias TodoItem =
     { title : String
-    , createdOn : String
-    , dueDate : Maybe String
-    , id : String
-    , description : String
+    , status : TodoStatus
+
+    -- , createdOn : String
+    -- , dueDate : Maybe String
+    -- , id : String
+    -- , description : String
     }
 
 
 type alias Model =
-    { todos : List TodoItem }
+    { view : Page, todos : List TodoItem, newTodo : String }
 
 
 initModel : Model
 initModel =
-    { todos = [] }
+    { view = Home
+    , newTodo = ""
+    , todos = []
+    }
